42ba8e8c2ee97061d39816784c02562cffd2c10e
abdullah-shwaiky <abdullahshwaiky@gmail.com>
2024-09-17 21:24:07 +0300

    Added views with authentication support

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from . import models
 from .serializers import BookingSerializer, MenuSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
@@ -11,6 +12,7 @@ def index(request):
     return render(request, 'index.html', {})
 
 class bookingView(APIView):
+    permission_classes = [IsAuthenticated]
     def get(self, request):
         items = models.Booking.objects.all()
         serializer = BookingSerializer(items, many = True)
@@ -19,11 +21,14 @@ class bookingView(APIView):
 class MenuItemView(ListCreateAPIView):
     queryset = models.Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
     
 class SingleMenuItemView(RetrieveAPIView, DestroyAPIView):
     queryset = models.Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
     
 class BookingViewSet(ModelViewSet):
     queryset = models.Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
