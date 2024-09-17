48996171b6292090b155104ffd3d57bc7122adda
abdullah-shwaiky <abdullahshwaiky@gmail.com>
2024-09-17 19:02:47 +0300

    Added views. What the fuck is Meta thinking here?

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('menu/', views.MenuItemView.as_view(), name = 'menu'),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view(), name = 'menu item'),
+    path('booking/', views.bookingView.as_view(), name = 'booking')
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import models
+from .serializers import BookingSerializer, MenuSerializer
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, DestroyAPIView
+from rest_framework.viewsets import ModelViewSet
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class bookingView(APIView):
+    def get(self, request):
+        items = models.Booking.objects.all()
+        serializer = BookingSerializer(items, many = True)
+        return Response(serializer.data)
+    
+class MenuItemView(ListCreateAPIView):
+    queryset = models.Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemView(RetrieveAPIView, DestroyAPIView):
+    queryset = models.Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(ModelViewSet):
+    queryset = models.Booking.objects.all()
+    serializer_class = BookingSerializer
